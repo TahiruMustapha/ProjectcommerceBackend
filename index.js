a6fd49c5c5a64f0de1ae557c7d0344c32f23d2a5
Mustapha tahiru <tahirumustapha13@gmail.com>
2025-02-04 15:54:49 +0000

    Added cors to index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const router = require('./routes')
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const router = require("./routes");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-const app = express()
-app.use(cors({
-    origin: "http://localhost:3000",  // Ensure no trailing slash here
-    credentials: true                // Allow credentials (cookies, HTTP authentication)
-}));
-app.use(express.json())
-app.use(cookieParser())
+app.use("/api", router);
 
-app.use("/api",router)
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT
-
-
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Server is running "+PORT)
-    })
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running " + PORT);
+  });
+});
