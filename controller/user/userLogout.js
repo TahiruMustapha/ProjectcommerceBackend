2ea8fb5a0e90dead8ccdfbe60fbe22dcca44a24d
Mustapha tahiru <tahirumustapha13@gmail.com>
2025-02-06 23:49:01 +0000

    Added changes

--- controller/user/userLogout.js
+++ controller/user/userLogout.js
@@ -1,21 +1,24 @@
-async function userLogout(req,res){
-    try{
-        res.clearCookie("token",{path : "/"})
+async function userLogout(req, res) {
+  try {
+    res.clearCookie("token", {
+      path: "/",
+      sameSite: "None",
+      secure: process.env.NODE_ENV === "development" ? false : true,
+    });
 
-        res.json({
-            message : "Logged out successfully",
-            error : false,
-            success : true,
-            data : []
-        })
-    }catch(err){
-        res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
-        })
-    }
+    res.json({
+      message: "Logged out successfully",
+      error: false,
+      success: true,
+      data: [],
+    });
+  } catch (err) {
+    res.json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
 }
 
-
-module.exports = userLogout
+module.exports = userLogout;
