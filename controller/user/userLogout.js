921378fe7a5e75e5d4ff6a75211fd7465d260d01
Mustapha tahiru <tahirumustapha13@gmail.com>
2025-02-12 22:48:53 +0000

    New fixes

--- controller/user/userLogout.js
+++ controller/user/userLogout.js
@@ -2,10 +2,10 @@ async function userLogout(req, res) {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      domain:
-        process.env.NODE_ENV === "production"
-          ? ".spectacular-kleicha-d2752f.netlify.app" // Frontend domain (with leading dot for subdomains)
-          : undefined,
+      // domain:
+      //   process.env.NODE_ENV === "production"
+      //     ? ".spectacular-kleicha-d2752f.netlify.app" // Frontend domain (with leading dot for subdomains)
+      //     : undefined,
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       secure: process.env.NODE_ENV === "production",
       path: "/",
