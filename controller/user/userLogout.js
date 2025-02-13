fb2a9c66889933d2f41ef8e27f24bc1adbbdefd7
Mustapha tahiru <tahirumustapha13@gmail.com>
2025-02-13 00:01:25 +0000

    New fixes

--- controller/user/userLogout.js
+++ controller/user/userLogout.js
@@ -1,5 +1,8 @@
 async function userLogout(req, res) {
   try {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate'); // All common directives
+    res.setHeader('Pragma', 'no-cache'); // For older browsers
+    res.setHeader('Expires', '0'); // Set expiration to a past date
     // console.log(req.cookies);
     const isProduction = process.env.NODE_ENV === "production";
     const domain = isProduction ? ".spectacular-kleicha-d2752f.netlify.app" : undefined; // Dynamic domain
@@ -8,7 +11,7 @@ async function userLogout(req, res) {
 
     res.clearCookie("token", {
       httpOnly: true,
-      // domain: domain, // Use the dynamically determined domain
+      domain: domain, // Use the dynamically determined domain
       sameSite: sameSite,
       secure: secure,
       path: "/",
